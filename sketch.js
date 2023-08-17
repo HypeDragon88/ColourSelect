a8fca2711801c66346af3a63633f0e1f495b960b
HypeDragon88 <142500420+HypeDragon88@users.noreply.github.com>
2023-08-17 17:10:34 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(1500,1000)
+  let person = prompt("What colour do you want the background to be", "red");
+  background(person)
+}
+
+
+
+function draw() 
+{
+
+
+}
+
+
+
+
